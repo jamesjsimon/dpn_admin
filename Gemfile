c1016ff2bc43b1f2a95508f1b7eb78d8cf844dee
James Simon <jjsimon@stanford.edu>
2013-07-23 11:43:48 -0700

    working?

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+require 'rubygems'
+require 'mongo'
+gem 'rails', '4.0.0.rc1'
+gem "mongo_mapper"
+gem 'sass-rails', '~> 4.0.0.rc1'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+# gem 'therubyracer', platforms: :ruby
+#gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.0.1'
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use Capistrano for deployment
+ gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = 'f76ebdfa6facd198044deefb9843360c3dcbe2010fe4747bbbcbe57f609894cd235cf94ed79f3bb387f60247fa7e7fe20e22d943b478df5c46d31893a2e02e7d'
